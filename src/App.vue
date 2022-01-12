8454e8f3a4450994e82d8ea5d4ec9b6d681a9377
Your Namevladbond00111 git config --global user.name Your <vladbond00111@gmail.com>
2022-01-12 22:50:34 +0200

    second

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
-
 </template>
 
 <script>
@@ -22,7 +21,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center
+  text-align: center;
   color: #2c3e50;
 
   margin-top: 60px;
